f6da4db00d1016d86107d4629576ec9c87fe6be9
sameer-in <sameer.inamdar@sysdig.com>
2022-10-28 11:55:26 -0700

    renaming module

--- examples/single-account/main.tf
+++ examples/single-account/main.tf
@@ -19,11 +19,10 @@ module "ssm" {
 # cspm
 #
 
-module "cspm-single-account" {
+module "cspm" {
   source = "../../modules/services/cspm"
   count  = var.deploy_cspm ? 1 : 0
-
-  name              = "${var.name}-cspm"
+  name   = "${var.name}-cspm"
   tags = var.tags
 }
 
