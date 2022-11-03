6af6a50509a039de2378811af6f3c5f01d53b099
sameer-in <sameer.inamdar@sysdig.com>
2022-11-03 15:03:13 -0700

    minor edits

--- examples/single-account-cspm/outputs.tf
+++ examples/single-account-cspm/outputs.tf
@@ -1,5 +1,4 @@
-output "cspm_role_arn" {
-  //  value       = length(module.cloudtrail) > 0 ? module.cloudtrail[0].cloudtrail_sns_arn : var.cloudtrail_sns_arn
+output "role_arn" {
   value       = module.cspm_single.cspm_role_arn
   description = "ARN of cspm role"
 }
--- modules/services/cspm/outputs.tf
+++ modules/services/cspm/outputs.tf
@@ -1,5 +1,4 @@
 output "cspm_role_arn" {
-  //  value       = length(module.cloudtrail) > 0 ? module.cloudtrail[0].cloudtrail_sns_arn : var.cloudtrail_sns_arn
   value       = aws_iam_role.cspm_role[0].arn
   description = "ARN of cspm role"
 }
