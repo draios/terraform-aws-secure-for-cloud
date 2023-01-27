a598950679b50acd2f169947c782ba0f3c20661d
sameer-in <77694703+sameer-in@users.noreply.github.com>
2023-01-27 15:00:42 -0800

    Update outputs.tf

--- examples/single-account-apprunner/outputs.tf
+++ examples/single-account-apprunner/outputs.tf
@@ -2,3 +2,7 @@ output "cloudtrail_sns_topic_arn" {
   value       = length(module.cloudtrail) > 0 ? module.cloudtrail[0].cloudtrail_sns_arn : var.cloudtrail_sns_arn
   description = "ARN of cloudtrail_sns topic"
 }
+output "role_arn" {
+  value       = module.cspm_single.cspm_role_arn
+  description = "ARN of cspm role"
+}
