d9ba2763acd763cd5304002be5a313891ec63376
sameer-in <sameer.inamdar@sysdig.com>
2023-04-20 17:26:15 -0700

    adding delay in role creation

--- modules/services/event-bridge/organizational.tf
+++ modules/services/event-bridge/organizational.tf
@@ -15,39 +15,17 @@ locals {
   region_set              = length(var.regions) == 0 ? [data.aws_region.current.name] : toset(var.regions)
 }
 
-# permission policy for stackset admin role
-resource "aws_iam_policy" "admin_role_policy" {
-  count  = var.is_organizational ? 1 : 0
-  name   = "AssumeRole-AWSCloudFormationStackSetExecutionRole"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "sts:AssumeRole"
-            ],
-            "Resource": [
-                "arn:*:iam::*:role/AWSCloudFormationStackSetExecutionRole"
-            ],
-            "Effect": "Allow"
-        }
-    ]
-}
-EOF
-}
-
-# policy attachment for stackset admin role
-resource "aws_iam_policy_attachment" "admin_role_attachment" {
-  count      = var.is_organizational ? 1 : 0
-  policy_arn = aws_iam_policy.admin_role_policy[0].arn
-  roles      = [aws_iam_role.mgmt_stackset_admin_role[0].name]
-  name       = "admin-role-policy-attachment"
-}
-
-# admin role needed to deploy resources in management account via stackset
+//# admin role needed to deploy resources in management account via stackset
 resource "aws_iam_role" "mgmt_stackset_admin_role" {
   count = var.is_organizational ? 1 : 0
+
+  # need to add local-exec block as role propagation is not instant. AWS needs some time to propagate role
+  # in every region inspiet of being a global resource. so we add a delay to make sure this role
+  # is available before creation of stackset
+  # https://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_general.html#troubleshoot_general_eventual-consistency
+  provisioner "local-exec" {
+    command = "sleep 3;"
+  }
 
   name = "AWSCloudFormationStackSetAdministrationRole"
   tags = var.tags
@@ -66,11 +44,38 @@ resource "aws_iam_role" "mgmt_stackset_admin_role" {
     ]
 }
 EOF
+  inline_policy {
+    name   = "AssumeRole-AWSCloudFormationStackSetExecutionRole"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Resource": [
+                "arn:*:iam::*:role/AWSCloudFormationStackSetExecutionRole"
+            ],
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+  }
 }
 
-# execution role needed to deploy resources in management account via stackset
+//# execution role needed to deploy resources in management account via stackset
 resource "aws_iam_role" "mgmt_stackset_execution_role" {
   count = var.is_organizational ? 1 : 0
+
+  # need to add local-exec block as role propagation is not instant. AWS needs some time to propagate role
+  # in every region inspiet of being a global resource. so we add a delay to make sure this role
+  # is available before creation of stackset
+  # https://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_general.html#troubleshoot_general_eventual-consistency
+  provisioner "local-exec" {
+    command = "sleep 3;"
+  }
 
   name = "AWSCloudFormationStackSetExecutionRole"
   tags = var.tags
@@ -82,7 +87,7 @@ resource "aws_iam_role" "mgmt_stackset_execution_role" {
         {
             "Effect": "Allow",
             "Principal": {
-                "AWS": "${aws_iam_role.mgmt_stackset_admin_role[0].unique_id}"
+                "AWS": "${aws_iam_role.mgmt_stackset_admin_role[0].arn}"
             },
             "Action": "sts:AssumeRole"
         }
@@ -134,7 +139,7 @@ resource "aws_cloudformation_stack_set" "mgmt-stackset" {
   permission_model        = "SELF_MANAGED"
   capabilities            = ["CAPABILITY_NAMED_IAM"]
   administration_role_arn = aws_iam_role.mgmt_stackset_admin_role[0].arn
-  depends_on              = [aws_iam_role.mgmt_stackset_execution_role[0]]
+  depends_on              = [aws_iam_role.mgmt_stackset_admin_role, aws_iam_role.mgmt_stackset_execution_role]
 
   template_body = <<TEMPLATE
 Resources:
