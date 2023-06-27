df600061db7bceae242296852fae379d0d11c5e8
Christopher Geers <christopher.geers@sysdig.com>
2023-06-27 15:01:20 -0500

    chore: build matrix functional tests

--- test/functional.sh
+++ test/functional.sh
@@ -4,7 +4,7 @@ set -e
 
 MOTO_VERSION=4.1.7
 MOTO_CONTAINER=motoserver
-EXAMPLES=$(find examples -type f -name main.tf)
+test -n "${EXAMPLES}" || EXAMPLES=$(find examples -type f -name main.tf)
 
 function cleanup() {
   docker rm -f ${MOTO_CONTAINER}
@@ -14,9 +14,10 @@ trap cleanup EXIT
 docker run --detach --name ${MOTO_CONTAINER} -p 5000:5000 motoserver/moto:${MOTO_VERSION}
 
 for example in ${EXAMPLES} ; do
+  printf "functional testing ${example}\n"
   example_dir="$(dirname ${example})"
   test -d "${example_dir}" || (printf "not an example directory: ${example_dir}\n" ; exit 1)
-  git clean -f -d "${example_dir}"
+  git clean -fxd "${example_dir}"
   pushd "${example_dir}"
     terraform init
     terraform validate
