4e5b726f15dc12629caf082b142ff3c30548c57f
Giovanni Recupero <giovanni.recupero@sysdig.com>
2023-07-24 16:53:21 +0200

    (AWS) Add first implementation of cloud-logs module

--- modules/services/cloud-logs/main.tf
+++ modules/services/cloud-logs/main.tf
@@ -1,11 +1,26 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Provider setup, the region is provided through a Terraform variable and has a default value.
+#-----------------------------------------------------------------------------------------------------------------------
 provider "aws" {
   region = var.region
 }
 
-data "aws_caller_identity" "current" {}
+#-----------------------------------------------------------------------------------------------------------------------
+# The only resource needed to make cloudingestion start to fetch data from the CloudTrail associated s3 bucket is a
+# properly set AWS IAM Role. Sysdig's trusted identity act as the Principal in the assume role Policy, namely the role
+# that CloudIngestion will use to assume the Client's role. At that point, given the permission set granted to the newly
+# created Role in the Client's account, CloudIngestion will be able to perform all the required actions in order to
+# list and fetch the log files that are automatically published in the target s3 bucket.
+#
+# Note: this setup assumes that the Client has already properly set up an AWS CloudTrail Trail and the associated bucket.
+# It is responsibility of Sysdig's Secure UI to provide the necessary information to make the Client perform the
+# necessary operations before applying the Terraform module.
+#-----------------------------------------------------------------------------------------------------------------------
 
-resource "aws_iam_role" "cloudlogs_s3_access" {
+# AWS IAM Role that will be used by CloudIngestion to access the CloudTrail-associated s3 bucket
+resource "aws_iam_role" "cloudlogs_access" {
   name               = var.role_name
+  tags               = var.tags
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -26,6 +41,7 @@ resource "aws_iam_role" "cloudlogs_s3_access" {
     ]
 }
 EOF
+
   inline_policy {
     name = "cloudlogs_s3_access_policy"
 
