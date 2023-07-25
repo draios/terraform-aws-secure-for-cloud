171aff336028989bfba65156b40511bec23c6b2c
mbreitung <mark.breitung@sysdig.com>
2023-07-25 10:53:37 -0700

    do not output anything for orgs

--- modules/services/agentless-scanning/outputs.tf
+++ modules/services/agentless-scanning/outputs.tf
@@ -1,17 +1,17 @@
 output "agentless_role_arn" {
-  description = "Role used by Sysdig BE for Secure Agentless"
-  value       = var.deploy_global_resources ? aws_iam_role.agentless[0].arn : ""
+  description = "Role used by Sysdig Platform for Secure Agentless"
+  value       = var.is_organizational ? null : var.deploy_global_resources ? aws_iam_role.agentless[0].arn : var.main_region_agentless_role_arn
 }
 
 output "kms_key" {
   description = "KMS key ID and ARN"
-  value = {
-    id  = var.is_organizational ? "" : aws_kms_key.agentless[0].key_id
-    arn = var.is_organizational ? "" : aws_kms_key.agentless[0].arn
+  value = var.is_organizational ? null : {
+    id  = aws_kms_key.agentless[0].key_id
+    arn = aws_kms_key.agentless[0].arn
   }
 }
 
 output "kms_key_alias" {
   description = "KMS key alias"
-  value       = aws_kms_alias.agentless
+  value       = var.is_organizational ? null : aws_kms_alias.agentless
 }
