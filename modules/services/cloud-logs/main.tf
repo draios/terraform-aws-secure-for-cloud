e31a711fb4f59b927053c5bf5657a8379935fdd4
Giovanni Recupero <giovanni.recupero@sysdig.com>
2023-07-27 12:03:45 +0200

    (AWS) Generalize documentation in cloud-logs module

--- modules/services/cloud-logs/main.tf
+++ modules/services/cloud-logs/main.tf
@@ -1,13 +1,13 @@
 #-----------------------------------------------------------------------------------------------------------------------
-# The only resource needed to make cloudingestion start to fetch data from the CloudTrail associated s3 bucket is a
+# The only resource needed to make Sysdig's backend start to fetch data from the CloudTrail associated s3 bucket is a
 # properly set AWS IAM Role. Sysdig's trusted identity act as the Principal in the assume role Policy, namely the role
-# that CloudIngestion will use to assume the Client's role. At that point, given the permission set granted to the newly
-# created Role in the Client's account, CloudIngestion will be able to perform all the required actions in order to
-# list and fetch the log files that are automatically published in the target s3 bucket.
+# that the backend will use to assume the Client's role. At that point, given the permission set granted to the newly
+# created Role in the Client's account, Sysdig's backend will be able to perform all the required actions in order to
+# retrieve the log files that are automatically published in the target s3 bucket.
 #
 # Note: this setup assumes that the Customer has already properly set up an AWS CloudTrail Trail and the associated bucket.
-# It is responsibility of Sysdig's Secure UI to provide the necessary information to make the Customer perform the
-# necessary operations before applying the Terraform module.
+# Sysdig's Secure UI provides the necessary information to make the Customer perform the
+# required setup operations before applying the Terraform module.
 #-----------------------------------------------------------------------------------------------------------------------
 
 # AWS IAM Role that will be used by CloudIngestion to access the CloudTrail-associated s3 bucket
