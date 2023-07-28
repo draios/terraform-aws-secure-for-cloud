a270bc82fec5c7ea1a646af52520a67b88d82975
Ravina Dhruve <ravina.dhruve@sysdig.com>
2023-07-28 10:47:02 -0700

    Fixing README for Agentless scanning
    - renaming variables from 'agentless' to 'scanning' in README
    - updated the input and output variables
    - removing default value for scanning_account_id and making it a required variable,
      since now the value will be coming from the BE

--- modules/services/agentless-scanning/variables.tf
+++ modules/services/agentless-scanning/variables.tf
@@ -11,7 +11,6 @@ variable "trusted_identity" {
 variable "scanning_account_id" {
   type        = string
   description = "The identifier of the account that will receive volume snapshots"
-  default     = "878070807337"
 }
 
 variable "kms_key_deletion_window" {
