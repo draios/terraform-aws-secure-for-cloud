674a818ec7e5a6e24c58a4682e894415e5e75dd8
alexeyovriakh <alexey.ovriakh@sysdig.com>
2024-02-08 11:01:26 +0000

    docs: Update variable descriptions

--- modules/services/eks/variables.tf
+++ modules/services/eks/variables.tf
@@ -1,20 +1,20 @@
 variable "region" {
-  description = "The AWS region where clusters reside"
+  description = "The AWS region where clusters are located"
   type        = string
 }
 
 variable "onboard_all_clusters" {
-  description = "Should all public clusters be onboarded and scanned by Sysdig"
+  description = "Set the value to true if all public clusters should be scanned by Sysdig"
   type        = bool
   default     = false
 }
 
 variable "clusters" {
-  description = "Names of the clusters to be onboarded and scanned by Sysdig"
+  description = "Please list the clusters to be scanned by Sysdig (when 'onboard_all_clusters' is set to false, only the clusters specified here will be scanned)"
   type        = set(string)
 }
 
 variable "principal_arn" {
-  description = "The IAM Principal ARN which will access the EKS cluster"
+  description = "Sysdig's IAM Principal ARN which will access the EKS clusters"
   type        = string
 }
