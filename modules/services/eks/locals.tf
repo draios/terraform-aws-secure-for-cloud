99a0b6e9c1b199acf95d1f1788a93f69c2e8d48a
alexeyovriakh <alexey.ovriakh@sysdig.com>
2024-02-12 14:34:53 +0000

    feat: Onboard opted-in clusters

--- modules/services/eks/locals.tf
+++ modules/services/eks/locals.tf
@@ -1,11 +1,18 @@
 locals {
   api_enabled_clusters = [
     for cluster in data.aws_eks_cluster.clusters :
-    cluster if contains(["API", "API_AND_CONFIG_MAP"], cluster.access_config[0].authentication_mode) && (var.onboard_all_clusters || contains(var.clusters, cluster.name))
+    cluster if contains(["API", "API_AND_CONFIG_MAP"], cluster.access_config[0].authentication_mode)
+  ]
+
+  tag_key = "sysdig:secure:scan"
+  tag_opt_in_values = ["true", "all"]
+  opted_in_clusters = [
+    for cluster in local.api_enabled_clusters :
+    cluster if var.onboard_all_clusters || contains(var.clusters, cluster.name) || contains(local.tag_opt_in_values, lookup(cluster.tags, local.tag_key, "false"))
   ]
 
   clusters = {
-    for cluster in local.api_enabled_clusters : cluster.name => cluster
+    for cluster in local.opted_in_clusters : cluster.name => cluster
     // Only onboard public clusters or clusters that Sysdig has access to
     if cluster.vpc_config[0].endpoint_public_access
   }
