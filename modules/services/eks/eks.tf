122da6ce59f3fb77d84c2849845d180c5403d8ff
alexeyovriakh <alexey.ovriakh@sysdig.com>
2024-03-07 14:53:12 +0000

    chore: Fix eks.tf formatting

--- modules/services/eks/eks.tf
+++ modules/services/eks/eks.tf
@@ -1,15 +1,15 @@
 resource "aws_eks_access_entry" "viewer" {
-  for_each        = var.clusters
-  cluster_name    = each.value 
-  principal_arn   = local.principal_arn // TODO: Use data source
-  type            = "STANDARD"
+  for_each      = var.clusters
+  cluster_name  = each.value
+  principal_arn = local.principal_arn // TODO: Use data source
+  type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "viewer" {
-  for_each        = var.clusters
-  cluster_name    = each.value
-  policy_arn      = local.policy_arn
-  principal_arn   = local.principal_arn // TODO: Use data source
+  for_each      = var.clusters
+  cluster_name  = each.value
+  policy_arn    = local.policy_arn
+  principal_arn = local.principal_arn // TODO: Use data source
   access_scope {
     type = "cluster"
   }
