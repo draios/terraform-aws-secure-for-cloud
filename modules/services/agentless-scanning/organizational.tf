3acd39aa9761cd288ef134b7c52c672e1abb511a
Christopher Geers <christopher.geers@sysdig.com>
2024-03-28 10:41:57 -0500

    fix: terraform fmt

--- modules/services/agentless-scanning/organizational.tf
+++ modules/services/agentless-scanning/organizational.tf
@@ -134,7 +134,7 @@ resource "aws_cloudformation_stack_set_instance" "scanning_role_stackset_instanc
     organizational_unit_ids = local.organizational_unit_ids
   }
   operation_preferences {
-    max_concurrent_count    = 10
+    max_concurrent_count = 10
   }
 }
 
