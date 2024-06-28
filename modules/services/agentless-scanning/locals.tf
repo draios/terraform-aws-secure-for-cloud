11c646395e8b83c7fce465828dd95b718afe1039
Jameel Al-Aziz <jameel@bastion.io>
2024-06-27 18:53:05 -0700

    fix(agentless-scanning): correct caller_arn for assumed roles
    When a role is assumed, the caller identity ARN is an ephemeral
    session-specific role. Rather than this role in IAM policies, we should
    use the underlying role that was assumed. This role can be retrieved
    with `aws_iam_session_context`. More info can be found in [this][1] AWS
    provider issue.
    
    [1]: https://github.com/hashicorp/terraform-provider-aws/issues/28381

--- modules/services/agentless-scanning/locals.tf
+++ modules/services/agentless-scanning/locals.tf
@@ -1,6 +1,10 @@
 data "aws_caller_identity" "current" {}
+data "aws_iam_session_context" "current" {
+  // Get the source role ARN from the currently assumed session role
+  arn = data.aws_caller_identity.current.arn
+}
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  caller_arn = data.aws_caller_identity.current.arn
+  caller_arn = data.aws_iam_session_context.current.issuer_arn
 }
